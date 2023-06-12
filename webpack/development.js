bc1a1495ed16aa5ffe5e557178892f76e76abc74
Wioletta Miazga <wioletta.miazga1992@gmail.com>
2023-06-12 11:32:57 +0200

    initial settings

--- webpack/development.js
+++ webpack/development.js
@@ -29,19 +29,7 @@ module.exports = {
         test: /\.(jpg|png|svg|gif|jpeg)$/,
         use: 'file-loader',
       },
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        options: {
-          presets: [
-            ["@babel/preset-env", { useBuiltIns: 'usage', corejs: "2.0.0" }]
-          ],
-          plugins: [
-            "@babel/plugin-proposal-class-properties"
-          ]
-        }
-      },
+
     ],
   },
 };
