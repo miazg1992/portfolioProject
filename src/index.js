0175df6e6f6e849b3c72bfadbfe86e68ce9454aa
Wioletta Miazga <wioletta.miazga1992@gmail.com>
2023-06-17 14:44:21 +0200

    create main scss sections

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // import "bootstrap/dist/js/bootstrap"
 import "./sass/main.scss";
-
+import './sass/fontawesome.scss'
 // Import all of Bootstrap's JS
 // import * as bootstrap from 'bootstrap'
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
